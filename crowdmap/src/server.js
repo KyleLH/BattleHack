2393fc1eb167550810d2ad38a0ac91dfad6e9663
Gowtam Lal <glal@enernoc.com>
2014-08-09 14:37:36 -0400

    Basic people map demo functioning.

--- crowdmap/src/server.js
+++ crowdmap/src/server.js
@@ -1,8 +1,54 @@
 var app = require('express')(),
     http = require('http').Server(app),
-    io = require('socket.io')(http);
+    io = require('socket.io')(http),
+    uuid = require('node-uuid');
 
+
+var clients = {};
 
 io.on('connection', function(socket) {
+    //console.log(require('util').inspect(socket, { depth: null, colors: true }));
 
+
+
+    var id = uuid.v4();
+
+    clients[id] = socket;
+
+    console.log('connected clients:' + Object.keys(clients).length);
+    //console.dir(clients);
+
+    socket.emit('log', 'Connected clients: ' + Object.keys(clients).length);
+
+    socket.on('coordinates', function(coordinates) {
+        console.log('got coordinates');
+        socket.coordinates = coordinates;
+
+        broadcast();
+    });
+
+    console.log('a user connected.');
+
+    socket.on('disconnect', function() {
+        delete clients[id];
+        console.log('user disconnected');
+        broadcast();
+    });
 });
+
+app.get('/', function(req, res) {
+    res.sendFile('index.html', { root: './public' });
+});
+
+http.listen(80, function() {
+    console.log('Listening on 80');
+});
+
+function broadcast() {
+    var coords = Object.keys(clients).map(function(id) {
+        return clients[id].coordinates;
+    });
+
+    io.emit('coordinates', coords);
+
+}
