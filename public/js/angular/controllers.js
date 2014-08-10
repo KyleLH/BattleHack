a1ba2b048eee63a393098eaf2a1e26e769829222
Kyle Holzinger <kylelholzinger@gmail.com>
2014-08-10 09:05:58 -0400

    Create modal when you click on create deal

--- public/js/angular/controllers.js
+++ public/js/angular/controllers.js
@@ -11,3 +11,20 @@ floc.controller('HomeController', function($scope, $route) {
     };
 
 });
+
+floc.controller('createDealController', function ($scope) {
+  console.log("testing");
+  $scope.createDeal = function () {
+    // $http.post("/deals", { address: $scope.newAddress, deal: $scope.newDeal, description: $scope.newDescription });
+    $scope.newAddress = '';
+    $scope.newDeal = '';
+    $scope.newDescription = '';
+    console.log("done");
+  };
+
+  $scope.clearDeal = function () {
+    $scope.newAddress = '';
+    $scope.newDeal = '';
+    $scope.newDescription = '';
+  }
+});
